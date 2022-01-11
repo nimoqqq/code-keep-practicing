6e569b050573b6ab0c2a45f8296aeab0ec44cd5c
chufan <907701820@qq.com>
2022-01-11 15:19:34 +0800

    first commit

---
+++ src/test/java/com/nimo/codekeeppracticing/CodeKeepPracticingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimo.codekeeppracticing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeKeepPracticingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
