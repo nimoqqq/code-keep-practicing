c7c81514408979d1a26e7bae798bacc32dc1b369
chuf <907701820@qq.com>
2022-04-08 10:05:54 +0800

    redis 工具类

---
+++ springboot-redis/src/main/java/com/nimo/redis/utils/RedisUtil.java
@@ -0,0 +1,13 @@
+package com.nimo.redis.utils;/**
+  * @program: code-keep-practicing
+  *
+  * @ClassName: RedisUtil
+  *
+  * @description: Redis 工具类
+  *
+  * @author: chuf
+  *
+  * @create: 2022-04-08 10:01
+  **/
+public class RedisUtil {
+}
