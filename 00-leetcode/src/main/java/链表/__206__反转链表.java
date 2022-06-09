28e052634f0290694406db6846d7dff4f28691a1
chuf <907701820@qq.com>
2022-06-09 10:16:45 +0800

    反转列表

---
+++ 00-leetcode/src/main/java/链表/__206__反转链表.java
@@ -0,0 +1,80 @@
+package 链表;
+
+/**
+ * https://leetcode.cn/problems/reverse-linked-list/
+ *
+ * @author : chuf
+ * @date : 2022-06-09
+ **/
+public class __206__反转链表 {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+//        ListNode listNode = reverseList(head);
+        ListNode listNode = reverseList1(head);
+        System.out.println(listNode);
+    }
+
+    /**
+     * 递归
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    /**
+     * 头插法
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList1(ListNode head) {
+        ListNode newListNode = null;
+
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = newListNode;
+            newListNode = head;
+            head = tmp;
+        }
+
+        return newListNode;
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
