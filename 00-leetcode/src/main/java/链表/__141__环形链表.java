e33634804992474f654ffb9f4fd64b159911cd95
chuf <907701820@qq.com>
2022-06-21 10:24:41 +0800

    算法：判断是否存在环形链表

--- 00-leetcode/src/main/java/链表/__141__环形链表.java
+++ 00-leetcode/src/main/java/链表/__141__环形链表.java
@@ -3,13 +3,26 @@ package 链表;
 /**
  * https://leetcode.cn/problems/linked-list-cycle/
  * 解决思路：快慢指针
+ *
  * @author : chuf
  * @date : 2022-06-21
  **/
 public class __141__环形链表 {
 
     public boolean hasCycle(ListNode head) {
-        return false;
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != slow) {
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 
     class ListNode {
