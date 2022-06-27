946093c2943981b3d0a84ccd93f1161aed8b408c
chuf <907701820@qq.com>
2022-06-27 23:44:20 +0800

    1

--- springboot-file-server/src/main/java/com/nimo/file/controller/UploadController.java
+++ springboot-file-server/src/main/java/com/nimo/file/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.nimo.file.controller;
 
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -31,12 +33,17 @@ public class UploadController {
         Integer chunks = null;
         String name = null;
         // 本地存储路径
-        String uploadPath = "/Users/chuf/Downloads";
+        String uploadPath = "/Users/chuf/Downloads/webuploader_demo-master";
         // 缓存流
         BufferedOutputStream os = null;
 
         try {
+            // 文件缓存
             DiskFileItemFactory factory = new DiskFileItemFactory();
+            factory.setSizeThreshold(1024);
+            factory.setRepository(new File(uploadPath));
+            //解析 request
+            ServletFileUpload fileUpload = new ServletFileUpload();
         }catch (Exception e){
             e.printStackTrace();
         }finally {
