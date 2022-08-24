2fb64120848994f8aac28f1a4f9fd2ac1c824112
chuf <907701820@qq.com>
2022-08-24 09:59:19 +0800

    移除链表元素

--- 00-leetcode/src/main/java/链表/__203__移除链表元素.java
+++ 00-leetcode/src/main/java/链表/__203__移除链表元素.java
@@ -14,22 +14,21 @@ public class __203__移除链表元素 {
         head.next = removeElements(head.next, val);
         return head.val == val ? head.next : head;
     }
-}
 
+    public static class ListNode {
+        int val;
+        ListNode next;
 
-class ListNode {
-    int val;
-    ListNode next;
+        ListNode() {
+        }
 
-    ListNode() {
-    }
+        ListNode(int val) {
+            this.val = val;
+        }
 
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
