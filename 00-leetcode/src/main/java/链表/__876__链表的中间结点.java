0543cce58fd4b219cc839ea814d897331a51197a
chuf <907701820@qq.com>
2022-08-24 10:37:41 +0800

    链表的中间结点

---
+++ 00-leetcode/src/main/java/链表/__876__链表的中间结点.java
@@ -0,0 +1,34 @@
+package 链表;
+
+/**
+ * @author : chuf
+ * @date : 2022-08-24
+ **/
+public class __876__链表的中间结点 {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
