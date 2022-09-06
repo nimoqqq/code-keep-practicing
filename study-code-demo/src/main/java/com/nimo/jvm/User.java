3bab335f77720bf0e936ca22581950ab1d5bebba
chuf <907701820@qq.com>
2022-09-06 16:51:07 +0800

    栈内创建对象

---
+++ study-code-demo/src/main/java/com/nimo/jvm/User.java
@@ -0,0 +1,37 @@
+package com.nimo.jvm;
+
+/**
+ * @author : chuf
+ * @date : 2022-09-06
+ **/
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
